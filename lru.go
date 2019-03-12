67b2dce31dc43327bb2ecb4e50923702b5d06367
Michael Cohen <scudette@gmail.com>
2019-03-12 04:45:46 +0000

    Added locks on reader lru.

--- lru.go
+++ lru.go
@@ -1,12 +1,12 @@
 // Based on github.com/hashicorp/golang-lru/simple but more optimized
-// for speed: by changing keys to int mapaccess is much faster. We also
-// remove thread locks because ntfs fds are not thread safe anyway.
+// for speed: by changing keys to int mapaccess is much faster.
 
 package ntfs
 
 import (
 	"container/list"
 	"errors"
+	"sync"
 )
 
 // EvictCallback is used to get a callback when a cache entry is evicted
@@ -18,6 +18,8 @@ type LRU struct {
 	evictList *list.List
 	items     map[int]*list.Element
 	onEvict   EvictCallback
+
+	mu sync.Mutex
 }
 
 // entry is used to hold a value in the evictList
@@ -42,6 +44,9 @@ func NewLRU(size int, onEvict EvictCallback) (*LRU, error) {
 
 // Purge is used to completely clear the cache.
 func (c *LRU) Purge() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	for k, v := range c.items {
 		if c.onEvict != nil {
 			c.onEvict(k, v.Value.(*entry).value)
@@ -53,6 +58,9 @@ func (c *LRU) Purge() {
 
 // Add adds a value to the cache.  Returns true if an eviction occurred.
 func (c *LRU) Add(key int, value interface{}) (evicted bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	// Check for existing item
 	if ent, ok := c.items[key]; ok {
 		c.evictList.MoveToFront(ent)
@@ -75,6 +83,9 @@ func (c *LRU) Add(key int, value interface{}) (evicted bool) {
 
 // Get looks up a key's value from the cache.
 func (c *LRU) Get(key int) (value interface{}, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	if ent, ok := c.items[key]; ok {
 		c.evictList.MoveToFront(ent)
 		return ent.Value.(*entry).value, true
@@ -85,6 +96,9 @@ func (c *LRU) Get(key int) (value interface{}, ok bool) {
 // Contains checks if a key is in the cache, without updating the recent-ness
 // or deleting it for being stale.
 func (c *LRU) Contains(key int) (ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	_, ok = c.items[key]
 	return ok
 }
@@ -92,6 +106,9 @@ func (c *LRU) Contains(key int) (ok bool) {
 // Peek returns the key value (or undefined if not found) without updating
 // the "recently used"-ness of the key.
 func (c *LRU) Peek(key int) (value interface{}, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	var ent *list.Element
 	if ent, ok = c.items[key]; ok {
 		return ent.Value.(*entry).value, true
@@ -102,6 +119,9 @@ func (c *LRU) Peek(key int) (value interface{}, ok bool) {
 // Remove removes the provided key from the cache, returning if the
 // key was contained.
 func (c *LRU) Remove(key int) (present bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	if ent, ok := c.items[key]; ok {
 		c.removeElement(ent)
 		return true
@@ -111,6 +131,9 @@ func (c *LRU) Remove(key int) (present bool) {
 
 // RemoveOldest removes the oldest item from the cache.
 func (c *LRU) RemoveOldest() (key int, value interface{}, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	ent := c.evictList.Back()
 	if ent != nil {
 		c.removeElement(ent)
@@ -122,6 +145,9 @@ func (c *LRU) RemoveOldest() (key int, value interface{}, ok bool) {
 
 // GetOldest returns the oldest entry
 func (c *LRU) GetOldest() (key int, value interface{}, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	ent := c.evictList.Back()
 	if ent != nil {
 		kv := ent.Value.(*entry)
@@ -132,6 +158,9 @@ func (c *LRU) GetOldest() (key int, value interface{}, ok bool) {
 
 // Keys returns a slice of the keys in the cache, from oldest to newest.
 func (c *LRU) Keys() []int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	keys := make([]int, len(c.items))
 	i := 0
 	for ent := c.evictList.Back(); ent != nil; ent = ent.Prev() {
@@ -143,6 +172,9 @@ func (c *LRU) Keys() []int {
 
 // Len returns the number of items in the cache.
 func (c *LRU) Len() int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	return c.evictList.Len()
 }
 
